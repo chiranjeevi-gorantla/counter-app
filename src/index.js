ef4714a3d954b638c0551696ab1d40e445771a03
chiranjeevi_gorantla <chiranjeevigorantla@gmail.com>
2023-01-30 19:58:01 -0800

    add counters, reset, navbar comps

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 
-const element = <h1>Original Gangster</h1>;
-
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
